2628946d6c9728fd09451fe7773e4977460eb122
FauziahAzura <fauziahazura068@gmail.com>
2025-03-13 16:40:53 +0700

    Add files via upload

---
+++ Pertemuan3/about.php
@@ -0,0 +1,34 @@
+<?php
+ include './top.php';
+ include './menu.php';
+ 
+ ?>
+ <!-- Page content wrapper-->
+ <div id="page-content-wrapper">
+     <?php
+     include './navbar.php';
+     ?>
+     
+     <!-- Page content-->
+     <div class="container-fluid">
+         <h1 class="mt-4">Halaman About</h1>
+         <p>Halo semuanya,</p>
+         <p>
+ 
+ Perkenalkan, nama saya Fauziah Nur Azura bisa juga di panggil ziah atau azura. Saya berasal dari Depok, Jawa Barat, Indonesia, saat ini saya berusia 19tahun. Saya sangat antusias untuk berbagi pengalaman dan pengetahuan dengan kalian semua.
+ 
+ Saya memiliki latar belakang pendidikan di bidang  Sistem informasi. Selain pendidikan, Saya punya satu fakta menarik tentang diri saya, yaitu saya sangat suka petualangan dan selalu berusaha untuk menemukan hal-hal baru dalam hidup, baik itu mencoba makanan baru atau menjelajahi tempat-tempat baru.
+ 
+ Saya memiliki sejumlah hobi yang membantu saya tetap aktif dan terinspirasi. Saya suka banyak hewan dan berolahraga terkhusus jogging, naik sepedah, dan badminton, karena itu membuat saya merasa lebih produktif dan bahagia. Selain itu, saya juga menikmati keterlibatan dalam kegiatan sosial seperti, kerja bakti, dan kegiatan sosial lainnya yang memberikan arti lebih dalam pada hidup saya.
+ 
+ Tujuan utama saya adalah terus belajar, berkembang, dan memberikan kontribusi yang positif, baik dalam pekerjaan saya maupun dalam komunitas yang saya ikuti. Saya percaya bahwa dengan kerja keras dan sikap positif, kita bisa mencapai hal-hal luar biasa.
+ 
+ Terima kasih telah membaca perkenalan saya. Saya sangat menantikan kesempatan untuk mengenal kalian lebih jauh dan bekerja sama dalam berbagai kesempatan ke depannya.
+ 
+ 
+         </p>
+     </div>
+ </div>
+ <?php
+ include './bottom.php';
+ ?>
